2f6a36def09956305b9a0cc316aff02ab6a52e38
Klaudia <gora.klaudia@gmail.com>
2019-12-13 22:43:16 +0100

    add cors

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const members = require('./routes/members');
 const auth = require('./routes/auth');
 const express = require('express');
 const config = require('config');
+const cors = require('cors');
 const app = express();
+
+// app.use(cors());
 
 if (!config.get('jwtPrivateKey')) {
     console.log('ERROR - jwtPrivateKey: Klucz prywatny nie został ustawiony')
@@ -17,12 +20,15 @@ mongoose.connect('mongodb://localhost/toDoList')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
+app.options('*', cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api/users', users);
 app.use('/api/projects', projects);
 app.use('/api/tasks', tasks);
 app.use('/api/members', members);
 app.use('/api/auth', auth);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,36 @@
 const {User} = require('../models/users'); 
 const Joi = require('joi');
-// const jwt = require('jsonwebtoken');
 const express = require('express');
 const bcrypt = require('bcryptjs');
-// const config = require('config');
 const router = express.Router();
+const cors = require('cors');
 
-router.post('/', async (req, res) => {
-    const { error } = validate(req.body);
-    if ( error ) 
-        return res.status(400).send(error.details[0].message);
-    
-    let user = await User.findOne({email: req.body.email});
-    if (!user) 
-        return res.status(400).send('Niepoprawny email lub hasło.');
-    
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    if (!validPassword)
-        return res.status(400).send('Niepoprawny email lub hasło.');
+router.post('/', cors(), async (req, res) => {
 
-    const token = user.generateAuthToken();
-    res.send(token);
+    try {
+        const { error } = validate(req.body);
+        if ( error ) 
+            return res.status(400).send(error.details[0].message);
+        
+        let user = await User.findOne({email: req.body.email});
+        if (!user) 
+            return res.status(400).send('Niepoprawny email lub hasło.');
+        
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        if (!validPassword)
+            return res.status(400).send('Niepoprawny email lub hasło.');
+
+        const token = user.generateAuthToken();
+        res
+          .status(200)
+          .set('Access-Control-Allow-Origin', '*')
+          .send({token: token, id: user.id});
+      } catch (err) {
+        console.log(err.message);
+        res.status(404).send(err.message);
+      }
+    
+      res.status(200).end('wyświetlono');
 });
 
 function validate(req){
